33cb4feac02ab2338aa337a778241836423e30eb
Fer Hernandez <fh.1111@gmail.com>
2019-01-21 22:43:12 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -9,6 +9,9 @@ class Admin::PreferencesController < ApplicationController
     redirect_to admin_preferences_path
   end
 
-  private
+  private 
 
-  def preference_params
+    def preference_params
+    params.require(:preference).permit(:allow_create_songs, :allow_create_artists, :song_sort_order, :artist_sort_order)
+    end
+end
